ef8f51d0c96bae6b6f6b69c0af363b5a6d1987ef
Sachin Rakibe <100836807+rakibesachin166@users.noreply.github.com>
2022-10-04 09:12:12 +0530

    Initial commit

---
+++ app/src/main/kotlin/eshop/shatir/customer/eshop_multivendor/MainActivity.kt
@@ -0,0 +1,8 @@
+package eshop.shatir.customer
+
+import io.flutter.embedding.android.FlutterActivity
+import io.flutter.embedding.android.FlutterFragmentActivity
+
+class MainActivity: FlutterFragmentActivity() {
+}
+
